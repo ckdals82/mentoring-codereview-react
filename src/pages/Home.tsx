f6eb40b2eaec3a9f83c152379483248ff12cf48e
ckdals82 <81973067+ckdals82@users.noreply.github.com>
2024-05-05 13:55:18 +0900

    Update Home.tsx
    홈화면 초기 진입시 전시회 리스트를 가져오거나
    
    전시회 혹은 찜목록 리스트를 가져오는 기능 구현.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,28 @@ import React, { useState } from 'react';
 import { getApiExhibitionList } from '@src/apis';
 import { ExhibitionWishlistView } from '@components/ExhibitionWishlistView';
 import { IExhibitionInfo } from '@../../types/index';
+import { ReactComponent as FilledStarIcon } from '@assets/icons/FilledStar.svg';
+import { ReactComponent as ExhibitionIcon } from '@assets/icons/ExhibitionLogo.svg';
 import { Flex } from 'antd';
+import styled from 'styled-components';
+import { useExhibitionStore } from '@src/Store';
 
 const Home = () => {
+  const { wishExhibitionList } = useExhibitionStore();
   const [exhibitionList, setExhibitionList] = useState<IExhibitionInfo[]>([]);
+  const [isWish, setIsWish] = useState<boolean>(false);
 
   const fetchData = async () => {
     // 초기 전시회 리스트만 불러오기 설정.
     try {
       const resData = await getApiExhibitionList();
-      setExhibitionList(resData);
+
+      if (isWish) {
+        const favoriteExhibitions = resData.filter((item) => wishExhibitionList.includes(item.id));
+        setExhibitionList(favoriteExhibitions);
+      } else {
+        setExhibitionList(resData);
+      }
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -19,17 +31,46 @@ const Home = () => {
 
   React.useEffect(() => {
     fetchData();
-  }, []);
+  }, [isWish]);
 
   return (
-    <Flex justify="center">
-      <Flex vertical={true}>
-        {exhibitionList.map((exhibtionData) => {
-          return <ExhibitionWishlistView viewData={exhibtionData} />;
-        })}
+    <>
+      <Flex justify="center">
+        <Flex vertical={true}>
+          {exhibitionList.map((exhibtionData) => {
+            return <ExhibitionWishlistView key={exhibtionData.id} viewData={exhibtionData} />;
+          })}
+        </Flex>
       </Flex>
-    </Flex>
+      <Flex justify="center">
+        <Footer>
+          <ExhibitionIcon
+            onClick={() => {
+              setIsWish(false);
+            }}
+          />
+          전시회
+        </Footer>
+        <Footer>
+          <FilledStarIcon
+            onClick={() => {
+              setIsWish(true);
+            }}
+          />
+          찜목록
+        </Footer>
+      </Flex>
+    </>
   );
 };
+const Footer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  width: 195px;
+  height: 79px;
+  top: 765px;
+`;
 
 export default Home;
